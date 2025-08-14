b8cebbcd3797d705734efbc96cc484501b2f5811
Chigan Madu <75643643+chigancc1@users.noreply.github.com>
2025-08-14 07:10:53 -0700

    Update W2LRootListController.m

--- W2LRootListController.m
+++ W2LRootListController.m
@@ -2,12 +2,11 @@
 
 #import "W2LRootListController.h"
 #import <Preferences/PSSpecifier.h>
-
-// Add the system headers you need:
+#import <notify.h>          // notify_post
 #import <UIKit/UIKit.h>
 #import <Foundation/Foundation.h>
-#import <spawn.h>        // posix_spawn / posix_spawnp
-#import <sys/wait.h>     // waitpid (optional, but recommended)
+#import <spawn.h>           // posix_spawn / posix_spawnp
+#import <sys/wait.h>        // waitpid
 
 @implementation W2LRootListController
 
@@ -22,20 +21,24 @@
     pid_t pid = 0;
     const char *argv[] = { "killall", "-9", "backboardd", NULL };
 
-    // Launch /usr/bin/killall backboardd
+    // Launch /usr/bin/killall backboardd, then wait to avoid zombies.
     int rc = posix_spawn(&pid, "/usr/bin/killall", NULL, NULL, (char * const *)argv, NULL);
     if (rc == 0) {
-        // Optionally wait for it to finish so the compiler/linker doesn't warn
-        // about an unused pid and to avoid zombie processes.
         (void)waitpid(pid, NULL, 0);
     }
 }
 
 - (void)openPickHelp {
-    NSURL *url = [NSURL URLWithString:@"file:///var/mobile/Media/W2Like/"];
-    [[UIApplication sharedApplication] openURL:url
-                                       options:@{}
-                             completionHandler:nil];
+    NSURL *url = [NSURL fileURLWithPath:@"/var/mobile/Media/W2Like/"];
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
+    if ([[UIApplication sharedApplication] respondsToSelector:@selector(openURL:options:completionHandler:)]) {
+        [[UIApplication sharedApplication] openURL:url options:@{} completionHandler:nil];
+    } else {
+        [[UIApplication sharedApplication] openURL:url];
+    }
+#pragma clang diagnostic pop
 }
 
 - (void)prefChanged:(id)sender {
