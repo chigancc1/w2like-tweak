7a87a4ec77c9786bbdf6ce0c059af70add030ced
Chigan Madu <75643643+chigancc1@users.noreply.github.com>
2025-08-14 06:56:29 -0700

    Update W2LRootListController.h

--- W2LRootListController.h
+++ W2LRootListController.h
@@ -1,5 +1,8 @@
-\
+// W2LRootListController.h
 #import <Preferences/PSListController.h>
 
 @interface W2LRootListController : PSListController
+- (void)respring;
+- (void)openPickHelp;
+- (void)prefChanged:(id)sender;
 @end
