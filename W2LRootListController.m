babba21699377eabbf133750a29cb796e38e87b3
Chigan Madu <75643643+chigancc1@users.noreply.github.com>
2025-08-14 06:52:31 -0700

    Update W2LRootListController.m

--- W2LRootListController.m
+++ W2LRootListController.m
@@ -1,7 +1,13 @@
-\
+// W2LRootListController.m
+
 #import "W2LRootListController.h"
 #import <Preferences/PSSpecifier.h>
-#import <notify.h>
+
+// Add the system headers you need:
+#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
+#import <spawn.h>        // posix_spawn / posix_spawnp
+#import <sys/wait.h>     // waitpid (optional, but recommended)
 
 @implementation W2LRootListController
 
@@ -13,14 +19,23 @@
 }
 
 - (void)respring {
-    pid_t pid;
-    const char* args[] = {"killall", "-9", "backboardd", NULL};
-    posix_spawn(&pid, "/usr/bin/killall", NULL, NULL, (char* const*)args, NULL);
+    pid_t pid = 0;
+    const char *argv[] = { "killall", "-9", "backboardd", NULL };
+
+    // Launch /usr/bin/killall backboardd
+    int rc = posix_spawn(&pid, "/usr/bin/killall", NULL, NULL, (char * const *)argv, NULL);
+    if (rc == 0) {
+        // Optionally wait for it to finish so the compiler/linker doesn't warn
+        // about an unused pid and to avoid zombie processes.
+        (void)waitpid(pid, NULL, 0);
+    }
 }
 
 - (void)openPickHelp {
     NSURL *url = [NSURL URLWithString:@"file:///var/mobile/Media/W2Like/"];
-    [[UIApplication sharedApplication] openURL:url options:@{} completionHandler:nil];
+    [[UIApplication sharedApplication] openURL:url
+                                       options:@{}
+                             completionHandler:nil];
 }
 
 - (void)prefChanged:(id)sender {
